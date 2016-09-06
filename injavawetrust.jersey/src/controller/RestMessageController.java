c7890230bba89d94953a51cd206ef8fd3ce7d6c7
levent <levent@ekmekTeknesi.ekmekTeknesi>
2016-09-06 23:56:48 +0300

    initial

---
+++ injavawetrust.jersey/src/controller/RestMessageController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/messages")
+public class RestMessageController {
+
+	@GET
+	@Path("/message")
+	public String getMessage() {
+
+		String message = "Welcome to injavawetrust.com Jersey Tutorial !";
+		return message;
+
+	}
+}
