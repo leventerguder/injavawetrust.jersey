8acfad886793def4c1e64f5d1468bba36eebb1a4
levent <levent@ekmekTeknesi.ekmekTeknesi>
2016-09-07 21:50:58 +0300

    web.xml

--- injavawetrust.jersey.register/src/service/RegisterApplication.java
+++ injavawetrust.jersey.register/src/service/RegisterApplication.java
@@ -1,22 +1,21 @@
 package service;
 
-
 import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
 import controller.RestMessageController;
 
-public class RegisterApplication extends Application{
+public class RegisterApplication extends Application {
 
-	   private Set<Object> singletons = new HashSet<Object>();
-	   
-	   public RegisterApplication() {
-	      singletons.add(new RestMessageController());
-	   }
-	   
-	   @Override
-	   public Set<Object> getSingletons() {
-	      return singletons;
-	   }
+	private Set<Object> singletons = new HashSet<Object>();
+
+	public RegisterApplication() {
+		singletons.add(new RestMessageController());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
 }
