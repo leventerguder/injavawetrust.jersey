60460aa8d9bce60dbe95dc37cb158be26f25757a
leventerguder <erguder.levent@gmail.com>
2016-09-25 17:41:04 +0300

    @FormParam

---
+++ injavawetrust.jersey.tutorial/src/_06/formParam/service/PersonService.java
@@ -0,0 +1,16 @@
+package _06.formParam.service;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/person-pathParam")
+public class PersonService {
+
+	@POST
+	@Path("/addPerson")
+	public String addPerson(@FormParam("name") String name, @FormParam("surname") String surname) {
+		String message = "addPerson is called. " + "Welcome , " + name + " " + surname;
+		return message;
+	}
+}
