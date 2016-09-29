583007c87f65e890dc4e8bd650fb0f029fe1ec92
leventerguder <erguder.levent@gmail.com>
2016-09-29 22:03:59 +0300

    cookieparam

--- injavawetrust.jersey.tutorial/src/_08/cookieParam/service/UserResource.java
+++ injavawetrust.jersey.tutorial/src/_08/cookieParam/service/UserResource.java
@@ -3,6 +3,9 @@ package _08.cookieParam.service;
 import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 
@@ -11,7 +14,7 @@ public class UserResource {
 
 	@GET
 	@Path("/addCookie")
-	//http://localhost:8080/injavawetrust.jersey.tutorial/user-cookieParam/addCookie
+	// http://localhost:8080/injavawetrust.jersey.tutorial/user-cookieParam/addCookie
 	public Response addCookie() {
 		String result = "addCookie is called.";
 		return Response.status(200).entity(result).cookie(new NewCookie("myCookie", "myCookieValue")).build();
@@ -19,10 +22,24 @@ public class UserResource {
 
 	@GET
 	@Path("/getCookie")
-	//http://localhost:8080/injavawetrust.jersey.tutorial/user-cookieParam/getCookie
-	public Response getCookie(@CookieParam("myCookie") String userAgent) {
+	// http://localhost:8080/injavawetrust.jersey.tutorial/user-cookieParam/getCookie
+	public Response getCookie(@CookieParam("myCookie") String cookie) {
 		String result = "getCookie is called. <br/>";
-		return Response.status(200).entity(result + " myCookie : " + userAgent).build();
+		return Response.status(200).entity(result + " myCookie : " + cookie).build();
+	}
+
+	@GET
+	@Path("/getCookieWithContext")
+	// http://localhost:8080/injavawetrust.jersey.tutorial/user-cookieParam/getCookie
+	public Response getCookieWithContext(@Context HttpHeaders headers) {
+
+		String result = "getCookieWithContext is called. <br/>";
+		for (String name : headers.getCookies().keySet()) {
+			Cookie cookie = headers.getCookies().get(name);
+			result = result + "Cookie: " + name + "=" + cookie.getValue() + "<br/>";
+		}
+
+		return Response.status(200).entity(result).build();
 	}
 
 }
