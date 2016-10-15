ba2d2032a1a952596e5f98066e62cfc11d5d1491
leventerguder <erguder.levent@gmail.com>
2016-10-16 01:27:08 +0300

    jaxb xml

---
+++ injavawetrust.jersey.tutorial/src/_13/jaxb/xml/model/Person.java
@@ -0,0 +1,43 @@
+package _13.jaxb.xml.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person {
+
+	@XmlElement
+	private int id;
+	@XmlElement
+	private String name;
+	@XmlElement
+	private String surname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+}
