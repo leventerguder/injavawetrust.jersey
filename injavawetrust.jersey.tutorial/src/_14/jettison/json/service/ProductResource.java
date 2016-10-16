67259393d257d63c6a1bebe30ec9185a43612edc
leventerguder <erguder.levent@gmail.com>
2016-10-16 16:34:19 +0300

    jettison

---
+++ injavawetrust.jersey.tutorial/src/_14/jettison/json/service/ProductResource.java
@@ -0,0 +1,26 @@
+package _14.jettison.json.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import _14.jettison.json.model.Product;
+
+@Path("/product-jettison-json")
+public class ProductResource {
+
+	@GET
+	@Path("/product")
+	@Produces(MediaType.APPLICATION_JSON)
+	//http://localhost:8080/injavawetrust.jersey.tutorial/product-jettison-json/product	
+	public Product getProduct() {
+		Product product = new Product();
+		product.setId(1);
+		product.setName("K-360");
+		product.setBrand("Logitech");
+		product.setPrice(99.99);
+		product.setCategory("Keyboard");
+		return product;
+	}
+}
