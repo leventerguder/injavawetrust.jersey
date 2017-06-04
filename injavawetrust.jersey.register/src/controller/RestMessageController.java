c17635e59d637fc5f08fde91667ec138f564fabc
leventerguder <erguder.levent@gmail.com>
2017-06-04 15:38:03 +0300

    review

--- injavawetrust.jersey.register/src/controller/RestMessageController.java
+++ injavawetrust.jersey.register/src/controller/RestMessageController.java
@@ -8,6 +8,7 @@ public class RestMessageController {
 
 	@GET
 	@Path("/message")
+	// http://localhost:8080/injavawetrust.jersey.register/application-subclass/messages/message
 	public String getMessage() {
 
 		String message = "Welcome to injavawetrust.com Jersey Tutorial !";
