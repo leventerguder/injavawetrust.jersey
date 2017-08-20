f8e32adec182b68165b15cc1bab9eafc8ffdbaa6
leventerguder <erguder.levent@gmail.com>
2017-08-20 20:17:31 +0300

    review

--- injavawetrust.jersey.tutorial/src/_22/client/api/test/LibraryClientAPITest2.java
+++ injavawetrust.jersey.tutorial/src/_22/client/api/test/LibraryClientAPITest2.java
@@ -13,11 +13,14 @@ public class LibraryClientAPITest2 {
 
 	public static void main(String[] args) {
 
-		String uri = "http://localhost:8080/injavawetrust.jersey.tutorial/library-client-api/book/100";
-		
+		// String uri = "http://localhost:8080/injavawetrust.resteasy.tutorial/library-client-api/book/100";
+		String uri = "http://localhost:8080/injavawetrust.resteasy.tutorial/library-client-api/book/{id}";
+
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target(uri);
+		// WebTarget target = client.target(uri);
+		WebTarget target = client.target(uri).resolveTemplate("id", "100");
 		Invocation.Builder invocationBuilder = target.request();
+		// The resolve Template() method fills in the id expression
 
 		// way1
 		Response response = invocationBuilder.get();
@@ -25,7 +28,8 @@ public class LibraryClientAPITest2 {
 
 		// way2
 		Response response2 = invocationBuilder.get();
-		Book book2 = response2.readEntity(new GenericType<Book>() {});
+		Book book2 = response2.readEntity(new GenericType<Book>() {
+		});
 
 		System.out.println("### way1 ###");
 		System.out.println(response.getStatus());
