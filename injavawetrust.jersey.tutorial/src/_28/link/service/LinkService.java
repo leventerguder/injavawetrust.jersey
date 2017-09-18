91c2e87e7bf5d922c4973badc1595c90fca6a607
leventerguder <erguder.levent@gmail.com>
2017-09-18 22:58:14 +0300

    jaxrs link

---
+++ injavawetrust.jersey.tutorial/src/_28/link/service/LinkService.java
@@ -0,0 +1,26 @@
+package _28.link.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Response;
+
+@Path("/link-path")
+public class LinkService {
+
+	@GET
+	@Path("/test1")
+	public Response test1LinkMethod() {
+		// rel : relation
+		// https://tools.ietf.org/html/rfc5988
+		Link link1 = Link.fromUri("http://localhost/root/customers/all").rel("section").type("text/plain").build();
+
+		Link link2 = Link.fromUri("http://{host}/root/customers/{id}").rel("self").type("text/plain").build("localhost",
+				"1234");
+
+		System.out.println(link1.toString());
+
+		Response response = Response.ok("Check Response Header!").links(link1, link2).build();
+		return response;
+	}
+}
