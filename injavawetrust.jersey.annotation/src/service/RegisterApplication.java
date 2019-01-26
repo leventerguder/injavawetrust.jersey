409163eaf4dcf664f08974a5aa35984127ced772
leventerguder <erguder.levent@gmail.com>
2019-01-26 15:14:13 +0300

    update

--- injavawetrust.jersey.annotation/src/service/RegisterApplication.java
+++ injavawetrust.jersey.annotation/src/service/RegisterApplication.java
@@ -1,25 +1,9 @@
 package service;
 
-
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import controller.MessageService;
-
 @ApplicationPath("/root-path")
-public class RegisterApplication extends Application{
-
-	   private Set<Object> singletons = new HashSet<Object>();
-	   
-	   public RegisterApplication() {
-	      singletons.add(new MessageService());
-	   }
-	   
-	   @Override
-	   public Set<Object> getSingletons() {
-	      return singletons;
-	   }
+public class RegisterApplication extends Application {
+	// We don't need to register resource classes.
 }
--- injavawetrust.jersey.register/src/service/RegisterApplication.java
+++ injavawetrust.jersey.register/src/service/RegisterApplication.java
@@ -1,21 +1,7 @@
 package service;
 
-import java.util.HashSet;
-import java.util.Set;
 import javax.ws.rs.core.Application;
 
-import controller.MessageService;
-
 public class RegisterApplication extends Application {
-
-	private Set<Object> singletons = new HashSet<Object>();
-
-	public RegisterApplication() {
-		singletons.add(new MessageService());
-	}
-
-	@Override
-	public Set<Object> getSingletons() {
-		return singletons;
-	}
+	// We don't need to register resource classes.
 }
